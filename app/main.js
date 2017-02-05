d142f0dfba287cc80e4168801048954976532c0f
Viktor Nemes <nemesv@gmail.com>
2017-02-05 20:55:49 +0100

    Display up to 10 random cicles

--- app/main.js
+++ app/main.js
@@ -1,9 +1,39 @@
+var counter;
+
 function setup() {
-    var canvas = createCanvas(100, 100);
+    var canvas = createCanvas(400, 400);
     canvas.parent('play');
-    background(233, 0, 0);
+    counter = new Counter();
+    counter.new();
 }
 
 function draw() {
-    ellipse(50, 50, 80, 80);
+    background(233, 0, 0);
+    counter.draw();
+}
+
+function keyTyped() {
+    if (key === ' ') {
+        counter.new();
+    }
+}
+
+function Counter () {
+    this.objects = [];
+    this.number = 0;
+   
+    this.new = function () {
+        this.objects = [];
+        this.number = random(1, 10);
+        for(var i = 1; i < this.number; i++) {
+            this.objects.push(createVector(random(400), random(400)));
+        }
+    }
+
+    this.draw = function() {
+        for(var i = 0; i < this.objects.length; i++) {
+            var object = this.objects[i];
+            ellipse(object.x, object.y, 80, 80);
+        }
+    }
 }
