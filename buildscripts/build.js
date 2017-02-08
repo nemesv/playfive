332e585144d29141552a58d820cf46b1773d539f
Viktor Nemes <nemesv@gmail.com>
2017-02-08 20:44:55 +0100

    Allow to pass in the destination dir as parameter to the build task

--- buildscripts/build.js
+++ buildscripts/build.js
@@ -1,18 +1,20 @@
 var fs = require('fs');
+var path = require('path');
 
 var copy = {
-    'app/main.js': 'dist/main.js',
-    'node_modules/p5/lib/p5.min.js': 'dist/p5.min.js',
-    'node_modules/p5/lib/addons/p5.dom.js': 'dist/p5.dom.js',
+    'app/main.js': 'main.js',
+    'node_modules/p5/lib/p5.min.js': 'p5.min.js',
+    'node_modules/p5/lib/addons/p5.dom.js': 'p5.dom.js',
 };
 
-if (!fs.existsSync('dist')) {
-    fs.mkdirSync('dist');
-}
+module.exports.copy = function (destination) {
+    destination = destination || 'dist';
+    if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination);
+    }
 
-module.exports.copy = function () {
     Object.keys(copy).forEach(key => {
-        fs.createReadStream(key).pipe(fs.createWriteStream(copy[key]));
+        fs.createReadStream(key).pipe(fs.createWriteStream(path.join(destination, copy[key])));
         console.log(`copied ${key}`);
     });
 }
