2db0c6462dd2c222ed849e9342d4df514bfda554
Viktor Nemes <nemesv@gmail.com>
2017-02-21 21:35:09 +0100

    Configure Babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,26 @@ module.exports = {
         filename: '[name].js',
         path: __dirname + '/dist'
     },
-    devServer:{
+    devServer: {
         publicPath: '/dist/'
     },
     plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            })
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components|dist|buildscripts)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015']
+                    }
+                }
+            }
         ]
+    }
 };
